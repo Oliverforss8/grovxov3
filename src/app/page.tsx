168ed8a5f0b2b2bae13031c3fb79c39673994f26
“oliverforss8” <150815568+Oliverforss8@users.noreply.github.com>
2024-08-14 08:54:18 +0200

    Push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,29 @@ import gro from "../../public/gro.png";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-row gap-14  items-center grid-container">
-      <div className="h-[85vh] w-full items-start flex flex-col justify-between">
+    <div className="min-h-screen flex flex-col md:flex-row gap-14 items-center grid-container">
+      <div className="h-[85vh] w-full flex flex-col justify-between">
         <div>
-          <Image src={gro} alt="Loop 1" width={1000} height={1000} />
+          <Image
+            src={gro}
+            alt="Loop 1"
+            layout="responsive"
+            width={1000}
+            height={1000}
+          />
+        </div>
+        <div className="md:hidden w-full h-full">
+          <VideoLoop />
         </div>
         <div className="text-center mt-4">
-          <p className="text-white font-bold text-[20px] text-left">
+          <p className="text-white font-bold text-[16px] md:text-[20px] text-left">
             Cultivating growth through community,
             <br /> mindset, and processes to enable driven <br /> individuals to
             reach their potential.
           </p>
         </div>
       </div>
-      <div className="w-full h-full z-10">
+      <div className="w-full h-full z-10 hidden md:block">
         <VideoLoop />
       </div>
     </div>
